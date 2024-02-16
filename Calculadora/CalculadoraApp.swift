df729add79f64002fbe47be5bf80c95202351043
guilhermepeppi <guilhermepeppi@gmail.com>
2024-02-16 10:07:58 -0300

    Initial Commit

---
+++ Calculadora/CalculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraApp.swift
+//  Calculadora
+//
+//  Created by Guilherme Peppi on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
