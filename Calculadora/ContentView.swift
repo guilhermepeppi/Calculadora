849d9d61ed472aca29cb01571d24726183b60659
guilhermepeppi <guilhermepeppi@gmail.com>
2024-02-16 11:58:35 -0300

    Upload

--- Calculadora/ContentView.swift
+++ Calculadora/ContentView.swift
@@ -8,14 +8,148 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var result = "0"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .trailing, spacing: 0) {
+            
+            HStack {
+                Text(result)
+                    .padding()
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+            }
+            
+            HStack {
+                
+                Button("7"){
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                
+                Button("8"){
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                
+                Button("9"){
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                Button("X"){
+                    
+                }
+                .padding(.vertical, 40)
+                .frame(maxWidth: .infinity)
+                .background(Color.orange)
+                
+            }
+            .foregroundColor(.white)
+            
+            HStack {
+                
+                Button("4"){
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                
+                Button("5"){
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                
+                Button("6"){
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                Button("-"){
+                    
+                }
+                .padding(.vertical, 40)
+                .frame(maxWidth: .infinity)
+                .background(Color.orange)
+                
+            }
+            .foregroundColor(.white)
+            
+            HStack {
+                
+                Button("1"){
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                
+                Button("2"){
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                
+                Button("3"){
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                Button("+"){
+                    
+                }
+                .padding(.vertical, 40)
+                .frame(maxWidth: .infinity)
+                .background(Color.orange)
+                
+            }
+            .foregroundColor(.white)
+            
+            GeometryReader { geometry in
+                HStack {
+                    
+                    Button("0"){
+                        
+                    }
+                    .padding()
+                    .frame(minWidth: geometry.size.width/2)
+                    
+                    
+                    Button(","){
+                        
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    
+                    
+                    Button("="){
+                        
+                    }
+                    .padding(.vertical, 40)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.orange)
+                    
+                }
+                .foregroundColor(.white)
+            }
+            .frame(maxHeight: 92)
+            
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .padding() //aaa
     }
 }
 
